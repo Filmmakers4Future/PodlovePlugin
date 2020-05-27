c7fce7305b6aec42c0da5a7733c2678d35d829b7
Paul-Vincent Roll <paviro@me.com>
2020-05-27 13:25:16 +0200

    Added some comments / restructured

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# Changelog
+
+## 0.1a0 (10.04.2020)
+### Features
+* initial podcastaudioplugin version
+
+## 0.2 (27.05.2020)
+### Features
+* initial podlove version
--- plugin.php
+++ plugin.php
@@ -1,14 +1,14 @@
 <?php
 
   /**
-    This is the PodlovePlayerPlugin plugin.
+    This is the PodlovePlugin plugin.
 
-    This file contains the PodlovePlayerPlugin plugin. 
+    This file contains the PodlovePlugin plugin. 
 
-    @package urlaube\PodlovePlayerPlugin
-    @version 0.1a0
-    @author  Yahe <hello@yahe.sh>
-    @since   0.1a0
+    @package Filmmakers4Future/PodlovePlugin
+    @version 0.2
+    @author  Paul-Vincent Roll <paul-vincent@filmmakersforfuture.org>
+    @since   0.2
   */
 
   // ===== DO NOT EDIT HERE =====
@@ -20,7 +20,26 @@
 
     // CONSTANTS
 
-    // todo 
+    const PODLOVE_AUDIOFILES = "podlove_audioFiles";
+    const PODLOVE_PODCASTTITLE = "podlove_podcastTitle";
+    const PODLOVE_PODCASTLINK = "podlove_podcastLink";
+    
+    const PODLOVE_TITLE = "podlove_title";
+    const PODLOVE_POSTER = "podlove_poster";
+    const PODLOVE_DURATION = "podlove_duration";
+    const PODLOVE_AUDIOSIZES = "podlove_audioSizes";
+    const PODLOVE_AUDIOTITLES = "podlove_audioTitles";
+    const PODLOVE_AUDIOMIME = "podlove_audioMimeTypes";
+    
+    const PODLOVE_DOWNLOADFILES = "podlove_downloadFiles";
+    const PODLOVE_DOWNLOADSIZES = "podlove_downloadSizes";
+    const PODLOVE_DOWNLOADTITLES = "podlove_downloadTitles";
+    const PODLOVE_DOWNLOADMIME = "podlove_downloadMimeTypes";
+    
+    const PODLOVE_CHAPTERTITLES = "podlove_chapterTitles";
+    const PODLOVE_CHAPTERSTARTS= "podlove_chapterStarts";
+    const PODLOVE_CHAPTERLINKS = "podlove_chapterLinks";
+    const PODLOVE_CHAPTERIMAGES = "podlove_chapterImages";
 
     // HELPER FUNCTIONS
 
@@ -28,45 +47,52 @@
       $result = value($item, CONTENT);
 
       if (is_string($result)) {
-        // we at least need the audio file
-        if (value($item, "podlove_audioFiles")) {
+        if (value($item, PODLOVE_AUDIOFILES)) {
+          
+          // Base episode configuration
           $episode = [
             "version" => "5",
             "show" => [
-              "title" => value($item, "podlove_podcastTitle"),
-              "link" => value($item, "podlove_podcastLink")
+              "title" => value($item, PODLOVE_PODCASTTITLE),
+              "link" => value($item, PODLOVE_PODCASTLINK)
             ],
-            "title" => value($item, "podlove_title"),
-            "poster" => value($item, "podlove_poster"),
-            "duration" => value($item, "podlove_duration"),
-            "link" => value($item, "podlove_link")
+            "title" => value($item, PODLOVE_TITLE),
+            "poster" => value($item, PODLOVE_POSTER),
+            "duration" => value($item, PODLOVE_DURATION),
+            "link" => absoluteurl(value($item, URI))
           ];
           
-          $audioFiles = explode(",", value($item, "podlove_audioFiles"));
-          $audioSizes = explode(",", value($item, "podlove_audioSizes"));
-          $audioTitles = explode(",", value($item, "podlove_audioTitles"));
-          $audioMimeTypes = explode(",", value($item, "podlove_audioMimeTypes"));
+          // Parse audio files from markdown
+          $audioFiles = explode(",", value($item, PODLOVE_AUDIOFILES));
+          $audioSizes = explode(",", value($item, PODLOVE_AUDIOSIZES));
+          $audioTitles = explode(",", value($item, PODLOVE_AUDIOTITLES));
+          $audioMimeTypes = explode(",", value($item, PODLOVE_AUDIOMIME));
           
-          $downloadFiles = explode(",", value($item, "podlove_downloadFiles"));
-          $downloadSizes = explode(",", value($item, "podlove_downloadSizes"));
-          $downloadTitles = explode(",", value($item, "podlove_downloadTitles"));
-          $downloadMimeTypes = explode(",", value($item, "podlove_downloadMimeTypes"));
+          // Parse download files from markdown (optional)
+          $downloadFiles = explode(",", value($item, PODLOVE_DOWNLOADFILES));
+          $downloadSizes = explode(",", value($item, PODLOVE_DOWNLOADSIZES));
+          $downloadTitles = explode(",", value($item, PODLOVE_DOWNLOADTITLES));
+          $downloadMimeTypes = explode(",", value($item, PODLOVE_DOWNLOADMIME));
           
-          $chapterTitles = explode(",", value($item, "podlove_chapterTitles"));
-          $chapterStarts = explode(",", value($item, "podlove_chapterStarts"));
-          $chapterLinks = explode(",", value($item, "podlove_chapterLinks"));
-          $chapterImages = explode(",", value($item, "podlove_chapterImages"));
+          // Parse chapters from markdown (optional)
+          $chapterTitles = explode(",", value($item, PODLOVE_CHAPTERTITLES));
+          $chapterStarts = explode(",", value($item, PODLOVE_CHAPTERSTARTS));
+          $chapterLinks = explode(",", value($item, PODLOVE_CHAPTERLINKS));
+          $chapterImages = explode(",", value($item, PODLOVE_CHAPTERIMAGES));
           
+          // Create arrays for data
           $audio = array();
           $files = array();
           $chapters = array();
           
+          // Add audio files to audio array
           foreach($audioFiles as $i => $audioFile) {
             $audio[] = ["url" => $audioFile,
                         "size" => $audioSizes[$i],
                         "title" => $audioTitles[$i],
                         "mimeType" => $audioMimeTypes[$i]
                         ];
+            // Also add the audio files to the download list
             $files[] = ["url" => $audioFile,
                         "size" => $audioSizes[$i],
                         "title" => "Episode: ".$audioTitles[$i],
@@ -74,6 +100,7 @@
                         ];
           };
           
+          // Add download files to files array
           foreach($downloadFiles as $i => $downloadFile) {
             $files[] = ["url" => $downloadFile,
                         "size" => $downloadSizes[$i],
@@ -82,6 +109,7 @@
                         ];
           };
           
+          // Add chapters to chapter array
           foreach($chapterTitles as $i => $chapterTitle) {
             $chapters[] = ["title" => $chapterTitle,
                         "start" => $chapterStarts[$i],
@@ -90,19 +118,25 @@
                         ];
           };
           
+          // Add new config arrays to episode array
           $episode["audio"] = $audio;
           $episode["files"] = $files;
           $episode["chapters"] = $chapters;
             
+          // Load config json from file and parse it
           $configJSON = file_get_contents(__DIR__."/defaults/config.json");
           $configArray = json_decode($configJSON, true);
                  
+          # Create random id for podlove webplayer div
           $divID =  "player_".substr(uniqid(rand(),1),0,15);
           
+          # Create div for podlove webplayer
           $podloveplayer = fhtml("<div id=\"%s\" class=\"align-content-center\"></div>".NL,
                                $divID);
 
+          # Add script to load podlove webplayer with our configuration                  
           $podloveplayer .= fhtml("<script>");
+          // Better way to do this? Could not use fhtml since it invalidates the json data
           $podloveplayer .= 'window.addEventListener("load",function(){window.podlovePlayer("#'.$divID.'", '.json_encode($episode).', '.json_encode($configArray).')},false);';
           $podloveplayer .= fhtml("</script>");
                                 
