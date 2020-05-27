79f6b8ebefb491e28d0211e2caac1bba3815151a
Paul-Vincent Roll <paviro@me.com>
2020-05-27 12:47:23 +0200

    Initial podlove commit

--- CHANGELOG.md
+++
@@ -1,5 +0,0 @@
-# Changelog
-
-## 0.1a0 (10.04.2020)
-### Features
-* initial version
--- plugin.php
+++ plugin.php
@@ -1,12 +1,11 @@
 <?php
 
   /**
-    This is the PodcastAudioPlugin plugin.
+    This is the PodlovePlayerPlugin plugin.
 
-    This file contains the PodcastAudioPlugin plugin. It provides an audio player
-    feature and a shortcode for the display of the audio player.
+    This file contains the PodlovePlayerPlugin plugin. 
 
-    @package urlaube\podcastaudioplugin
+    @package urlaube\PodlovePlayerPlugin
     @version 0.1a0
     @author  Yahe <hello@yahe.sh>
     @since   0.1a0
@@ -17,69 +16,98 @@
   // prevent script from getting called directly
   if (!defined("URLAUBE")) { die(""); }
 
-  class PodcastAudioPlugin extends BaseSingleton implements Plugin {
+  class PodlovePlayerPlugin extends BaseSingleton implements Plugin {
 
     // CONSTANTS
 
-    const PODCAST_IMAGE = "podcast_image";
-
-    const AUDIOIMAGE    = "audioimage";
-    const ENCLOSURE     = "enclosure";
-    const ENCLOSURETYPE = "enclosuretype";
-    const IMAGE         = "image";
+    // todo 
 
     // HELPER FUNCTIONS
 
-    protected static function getAudioPlayer($item) {
+    protected static function getPodlovePlayer($item) {
       $result = value($item, CONTENT);
 
       if (is_string($result)) {
-        // retrieve the audio file
-        $audiofile = value($item, self::ENCLOSURE);
-        $audiotype = value($item, self::ENCLOSURETYPE);
-
-        // retrieve the background image
-        $background = value($item, self::AUDIOIMAGE);
-        if (null === $background) {
-          $background = value($item, self::IMAGE);
-        }
-        if (null === $background) {
-          $background = value(Handlers::class, self::PODCAST_IMAGE);
-        }
-
         // we at least need the audio file
-        if (null !== $audiofile) {
-          // generate audio player source code
-          $audioplayer = fhtml("<link href=\"%s\" rel=\"stylesheet\">".NL,
-                               path2uri(__DIR__."/css/style.css"));
+        if (value($item, "podlove_audioFiles")) {
+          $episode = [
+            "version" => "5",
+            "show" => [
+              "title" => value($item, "podlove_podcastTitle"),
+              "link" => value($item, "podlove_podcastLink")
+            ],
+            "title" => value($item, "podlove_title"),
+            "poster" => value($item, "podlove_poster"),
+            "duration" => value($item, "podlove_duration"),
+            "link" => value($item, "podlove_link")
+          ];
+          
+          $audioFiles = explode(",", value($item, "podlove_audioFiles"));
+          $audioSizes = explode(",", value($item, "podlove_audioSizes"));
+          $audioTitles = explode(",", value($item, "podlove_audioTitles"));
+          $audioMimeTypes = explode(",", value($item, "podlove_audioMimeTypes"));
+          
+          $downloadFiles = explode(",", value($item, "podlove_downloadFiles"));
+          $downloadSizes = explode(",", value($item, "podlove_downloadSizes"));
+          $downloadTitles = explode(",", value($item, "podlove_downloadTitles"));
+          $downloadMimeTypes = explode(",", value($item, "podlove_downloadMimeTypes"));
+          
+          $chapterTitles = explode(",", value($item, "podlove_chapterTitles"));
+          $chapterStarts = explode(",", value($item, "podlove_chapterStarts"));
+          $chapterLinks = explode(",", value($item, "podlove_chapterLinks"));
+          $chapterImages = explode(",", value($item, "podlove_chapterImages"));
+          
+          $audio = array();
+          $files = array();
+          $chapters = array();
+          
+          foreach($audioFiles as $i => $audioFile) {
+            $audio[] = ["url" => $audioFile,
+                        "size" => $audioSizes[$i],
+                        "title" => $audioTitles[$i],
+                        "mimeType" => $audioMimeTypes[$i]
+                        ];
+            $files[] = ["url" => $audioFile,
+                        "size" => $audioSizes[$i],
+                        "title" => "Episode: ".$audioTitles[$i],
+                        "mimeType" => $audioMimeTypes[$i]
+                        ];
+          };
+          
+          foreach($downloadFiles as $i => $downloadFile) {
+            $files[] = ["url" => $downloadFile,
+                        "size" => $downloadSizes[$i],
+                        "title" => $downloadTitles[$i],
+                        "mimeType" => $downloadMimeTypes[$i]
+                        ];
+          };
+          
+          foreach($chapterTitles as $i => $chapterTitle) {
+            $chapters[] = ["title" => $chapterTitle,
+                        "start" => $chapterStarts[$i],
+                        "href" => $chapterLinks[$i],
+                        "image" => $chapterImages[$i]
+                        ];
+          };
+          
+          $episode["audio"] = $audio;
+          $episode["files"] = $files;
+          $episode["chapters"] = $chapters;
+            
+          $configJSON = file_get_contents(__DIR__."/defaults/config.json");
+          $configArray = json_decode($configJSON, true);
+                 
+          $divID =  "player_".substr(uniqid(rand(),1),0,15);
+          
+          $podloveplayer = fhtml("<div id=\"%s\" class=\"align-content-center\"></div>".NL,
+                               $divID);
 
-          // insert the background image
-          if (null !== $background) {
-            $audioplayer .= fhtml("<div class=\"podcastaudio podcastaudio-image\">".NL.
-                                  "  <img src=\"%s\" alt=\"podcast image\">".NL,
-                                  $background);
-          } else {
-            $audioplayer .= fhtml("<div class=\"podcastaudio\">".NL);
-          }
-
-          // insert the audio file
-          if (null !== $audiotype) {
-            $audioplayer .= fhtml("  <audio controls preload=\"metadata\">".NL.
-                                  "    <source src=\"%s\" type=\"%s\">".NL.
-                                  "  </audio>".NL.
-                                  "</div>",
-                                  $audiofile,
-                                  $audiotype);
-          } else {
-            $audioplayer .= fhtml("  <audio controls preload=\"metadata\">".NL.
-                                  "    <source src=\"%s\">".NL.
-                                  "  </audio>".NL.
-                                  "</div>",
-                                  $audiofile);
-          }
-
-          // replace shortcode with audio player
-          $result = str_ireplace("[podcastaudio]", $audioplayer, $result);
+          $podloveplayer .= fhtml("<script>");
+          $podloveplayer .= 'window.addEventListener("load",function(){window.podlovePlayer("#'.$divID.'", '.json_encode($episode).', '.json_encode($configArray).')},false);';
+          $podloveplayer .= fhtml("</script>");
+                                
+          // replace shortcode with podlove player
+          $result = str_ireplace("[podloveplayer]", $podloveplayer, $result);
         }
       }
 
@@ -93,7 +121,7 @@
 
       if ($result instanceof Content) {
         if ($result->isset(CONTENT)) {
-          $result->set(CONTENT, static::getAudioPlayer($result));
+          $result->set(CONTENT, static::getPodlovePlayer($result));
         }
       } else {
         if (is_array($result)) {
@@ -101,7 +129,7 @@
           foreach ($result as $result_item) {
             if ($result_item instanceof Content) {
               if ($result_item->isset(CONTENT)) {
-                $result_item->set(CONTENT, static::getAudioPlayer($result_item));
+                $result_item->set(CONTENT, static::getPodlovePlayer($result_item));
               }
             }
           }
@@ -114,4 +142,4 @@
   }
 
   // register plugin
-  Plugins::register(PodcastAudioPlugin::class, "plugin", FILTER_CONTENT);
+  Plugins::register(PodlovePlayerPlugin::class, "plugin", FILTER_CONTENT);
